42d3437fb9df24f25786c2da1d1c661dd7a1d754
tonggan198699 <gantong1986@gmail.com>
2017-12-06 21:41:48 +0000

    Extending layout app to blade file

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', 'DashboardController@index');
+
+Route::get('/main', function () {
+    return view('main');
 });
